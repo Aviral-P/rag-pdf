96e8d85b59d61f1caaca1695139bb020221c34e4
Aviral Prakash <prakashaviral05@gmail.com>
2026-06-22 00:58:52 +0530

    Created retreival pipeline

--- index.py
+++ index.py
@@ -7,29 +7,27 @@ from langchain_qdrant import QdrantVectorStore
 
 load_dotenv()
 
-pdf_path=Path(__file__).parent/"nodejs.pdf"
+pdf_path = Path(__file__).parent / "nodejs.pdf"
 
-#Load this file in python program
+# Load this file in python program
 loader = PyPDFLoader(file_path=pdf_path)
-docs=loader.load()
+docs = loader.load()
 
-#Split the docs into smaller chunks
+# Split the docs into smaller chunks
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=1000,
     chunk_overlap=400,
 )
 chunks = text_splitter.split_documents(documents=docs)
 
-#Vector Embeddings
-embedding_model = OpenAIEmbeddings(
-    model = "text-embedding-3-large"
-)
+# Vector Embeddings
+embedding_model = OpenAIEmbeddings(model="text-embedding-3-large")
 
-vector_store=QdrantVectorStore.from_documents(
+vector_store = QdrantVectorStore.from_documents(
     documents=chunks,
     embedding=embedding_model,
     url="http://localhost:6333",
-    collection_name="RAG"
+    collection_name="RAG",
 )
 
 print("Indexing of documents done!")
