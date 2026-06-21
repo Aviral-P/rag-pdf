cff17c7ef07856f726baf204457eea03be1254a5
Aviral Prakash <prakashaviral05@gmail.com>
2026-06-21 23:51:25 +0530

    Created chunks

--- index.py
+++ index.py
@@ -1,8 +1,18 @@
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 pdf_path=Path(__file__).parent/"nodejs.pdf"
 
 #Load this file in python program
 loader = PyPDFLoader(file_path=pdf_path)
 docs=loader.load()
+
+#Split the docs into smaller chunks
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=400,
+)
+chunks = text_splitter.split_documents(documents=docs)
+
+#Vector Embeddings
